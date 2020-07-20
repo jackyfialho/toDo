7e735c0ad7d252f4c2bc8e979c55bea0ddcef1df
jackyfialho <teixeijf@gmail.com>
2020-07-20 17:25:28 -0300

    primeiros passos

---
+++ backend/src/controller/TaskController.js
@@ -0,0 +1,20 @@
+const Task = require('../model/TaskModel');
+const TaskModel = require('../model/TaskModel');
+
+class TaskController{
+
+    async create(request, response) {
+        const task = new TaskModel(request.body);
+        await task
+                .save()
+                .then(response => {
+                    return response.status(200).json(response);
+                })
+                .catch(error => {
+                    return response.status(500).json(error);
+                })
+    };
+
+};
+
+module.exports = new TaskController();
